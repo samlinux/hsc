262e58bfc2a94c31cb32c180649d3e19228f68c0
Roland Bole <rbole@samlinux.at>
2020-05-09 06:18:01 +0000

    modify the fetch process of the channel information on peer1

--- examples/e01.6/create.sh
+++ examples/e01.6/create.sh
@@ -2,6 +2,8 @@
 # to stop the network call docker-compose down
 # to start the network call docker-compose start
 
+# clear chaincode container
+docker rm -f $(docker ps -a | awk '($2 ~ /dev-peer.*/) {print $1}')
 
 # clear for restart network if not persistent
 # you can clear da network data, but not the artifacts
@@ -13,6 +15,7 @@ sudo rm ./ca-mars.morgen.net/peers/peer1/assets/channel1.block
 
 sudo rm -R ./ca-mars.morgen.net/peers/peer0/production/
 sudo rm -R ./ca-mars.morgen.net/peers/peer1/production/
+sudo rm -R ./ca-orderer.morgen.net/orderer/production/
 
 ## if you want to start from the ground clear all artifacts
 ## ---- BE CAREFULLY -----
@@ -66,25 +69,16 @@ export CORE_PEER_TLS_ROOTCERT_FILE="/tmp/hyperledger/mars.morgen.net/peers/peer0
 export CORE_PEER_ADDRESS="peer0.mars.morgen.net:7051"
 
 peer channel create -c channel1 -f /tmp/hyperledger/mars.morgen.net/peers/peer0/assets/channel.tx -o orderer.morgen.net:7050 --outputBlock /tmp/hyperledger/mars.morgen.net/peers/peer0/assets/channel1.block --tls --cafile /tmp/hyperledger/mars.morgen.net/peers/peer0/tls-msp/tlscacerts/tls-0-0-0-0-7052.pem
-
 peer channel join -b /tmp/hyperledger/mars.morgen.net/peers/peer0/assets/channel1.block
-exit
 
-cp ./ca-mars.morgen.net/peers/peer0/assets/channel1.block ./ca-mars.morgen.net/peers/peer1/assets/channel1.block
-
-### peer1
-docker exec -it cli-mars.morgen.net bash 
-export CORE_PEER_LOCALMSPID="marsMSP"
-export CORE_PEER_MSPCONFIGPATH="/tmp/hyperledger/mars.morgen.net/admin/msp"
 export CORE_PEER_TLS_ROOTCERT_FILE="/tmp/hyperledger/mars.morgen.net/peers/peer1/tls-msp/tlscacerts/tls-0-0-0-0-7052.pem"
 export CORE_PEER_ADDRESS="peer1.mars.morgen.net:7051"
 
+# if you have to copy the file per hand, but this is't needed because usally you can fetch the channel configuration from the latest block
+#cp ./ca-mars.morgen.net/peers/peer0/assets/channel1.block ./ca-mars.morgen.net/peers/peer1/assets/channel1.block
 
-# not working at the moment ?? why ??
-## peer channel fetch newest channel1.block -c channel1 --orderer orderer.morgen.net:7050
-
+peer channel fetch newest /tmp/hyperledger/mars.morgen.net/peers/peer1/assets/channel1.block -c channel1 --orderer orderer.morgen.net:7050 --tls --cafile /tmp/hyperledger/mars.morgen.net/peers/peer0/tls-msp/tlscacerts/tls-0-0-0-0-7052.pem 
 peer channel join -b /tmp/hyperledger/mars.morgen.net/peers/peer1/assets/channel1.block
-
 
 ## Install chaincode on peer0
 export CORE_PEER_LOCALMSPID="marsMSP"
@@ -99,11 +93,8 @@ peer chaincode list --installed
 # instantiate the chaincode (only on peer0)
 peer chaincode instantiate -n sacc -v 1.0 -o orderer.morgen.net:7050 -C channel1  -c '{"Args":["msg","hello blockchain"]}' --tls --cafile /tmp/hyperledger/mars.morgen.net/peers/peer0/tls-msp/tlscacerts/tls-0-0-0-0-7052.pem
 
-
 peer chaincode query -n sacc -c '{"Args":["query","msg"]}' -C channel1 --tls --cafile /tmp/hyperledger/mars.morgen.net/peers/peer0/tls-msp/tlscacerts/tls-0-0-0-0-7052.pem
-
 peer chaincode invoke -n sacc -c '{"Args":["set", "msg","hello fabric"]}' -C channel1  --tls --cafile /tmp/hyperledger/mars.morgen.net/peers/peer0/tls-msp/tlscacerts/tls-0-0-0-0-7052.pem
-
 
 ## change to peer1
 export CORE_PEER_TLS_ROOTCERT_FILE="/tmp/hyperledger/mars.morgen.net/peers/peer1/tls-msp/tlscacerts/tls-0-0-0-0-7052.pem"
